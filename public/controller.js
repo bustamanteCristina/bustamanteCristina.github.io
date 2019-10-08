53a3c8a6f3bbf09c8b055a9a3212b991decbbfde
ahjook <merrycris.ajoc@student.passerellesnumeriques.org>
2019-10-08 20:46:21 +0800

    Initial commit

--- public/controller.js
+++ public/controller.js
@@ -1,100 +1,23 @@
-// basic functionalities
 var client;
+var topic = "cristina/device/status";
 
-var btnPublish = $("#publish-btn")
+client = mqtt.connect("wss://test.mosquitto.org:8081/mqtt");
+var d = new Date();
 
+var month = d.getMonth() + 1;
+var day = d.getDate();
 
-
-$('#btn-connect').on('click', function () {
-  // connect
-  console.log("connect button clicked..")
-  client = mqtt.connect("wss://test.mosquitto.org:8081/mqtt")
-  $("#status").text("Connecting....")
-  $("#status").css("color", "red")
-  $("#status").css("font-style", "italic")
-  $("#status").css("font-weight", "bold")
- 
-  client.on("connect", function () {
-    console.log("Success")
-    $("#status").text("Connected Successfully!")
-    $("#status").css("color", "green")
-    $("#status").css("font-style", "italic")
-    $("#status").css("font-weight", "bold")
-    
-  });// end connect
-
-  $(".btn-disconnect").click(function () {
-    client.end();
-    $("#status").text("Disconnected")
-    $("#status").css("color", "red")
-  })//end disconnect
-
-  //Publish 
-  
-  $("#btn-pub").click(function () {
-    var topic = $("#topic").val();
-    var message = $("#message").val();
-    if (topic == "" || message == "") {
-      Swal.fire({
-        type: 'error',
-        title: 'All Input is Required',
-      })
-    } else {
-      console.log("Published Topic: "+topic+ " Message: " + message)
-      client.publish(topic, message);
-      Swal.fire({
-        type: 'success',
-        title: 'Publish Successfully!',
-      })
-    }
-  })
-
-  //Subscribe
-  $("#btn-sub").click(function () {
-    var topsub = $("#topic-sub").val();
-    if (topsub == "") {
-      Swal.fire({
-        type: 'error',
-        title: 'Topic is Required',
-      })
-    } else {
-      console.log("Subcribed Topic: "+topsub)
-      client.subscribe(topsub);
-      Swal.fire({
-        type: 'success',
-        title: 'Subscribe Successfully',
-      })
-    }
-  })
-  $("#btn-unsub").click(function () {
-    var topsub = $("#topic-sub").val();
-    if (topsub == "") {
-      Swal.fire({
-        type: 'error',
-        title: 'Topic is Required',
-      })
-    } else {
-      client.unsubscribe(topsub);
-      Swal.fire({
-        type: 'success',
-        title: 'Unsubscribe Successfully',
-      })
-    }
-    $("#btn-unsub").removeClass("alert-success")
-    $("#btn-unsub").addClass("alert-secondary")
-  })//end unsubscribe
-
-  //Message
-  client.on("message", function (topic, payload) {
-    console.log("Recieved Topic: "+topic+"Payload: "+payload)
-    var row = $("<tr>")
-    $("<td>").text(topic).appendTo($(row))
-    $("<td>").text(payload).appendTo($(row))
-    $("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row))
-    $("tbody").append($(row))
-    
-  })
-})//end of click
-
-
-
+var time = d.getFullYear() + '/' +
+  (month < 10 ? '0' : '') + month + '/' +
+  (day < 10 ? '0' : '') + day;
+// var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+var payload = "turned on : " + time;
+var payloadOff = "turned off : " + time;
+$("#on").click(function () {
+  $("h2").text("The device is now turned on...");
+  client.publish(topic, payload);
+});
+$("#off").click(function () {
+  $("h2").text("The device is now turned off...");
+  client.publish(topic, payloadOff);
+});
